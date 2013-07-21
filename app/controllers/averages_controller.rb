61ac39412cb5dc2e9c480e0f0b022efe07260366
Simon König <koenig_simon@gmx.de>
2013-07-21 18:44:32 +0200

    bootstrap styling

--- app/controllers/averages_controller.rb
+++ app/controllers/averages_controller.rb
@@ -1,6 +1,8 @@
 require 'will_paginate/array'
 
 class AveragesController < ReportController
+  WillPaginate.per_page = 20
+
   def show
     @games = []
     Game.joins(:match_day).select('player_id, COUNT(*) AS game_count, AVG(points) AS points, AVG(cleared_frames) AS cleared_frames, AVG(strikes) AS strikes, AVG(spares) AS spares, AVG(splits) AS splits, AVG(cleared_splits) AS cleared_splits, AVG(fouls) AS fouls').where(:player_id => @player_restrictions, 'match_days.location_id' => @location_restrictions, 'match_days.category_id' => @category_restrictions).where('match_days.match_day >= ? AND match_days.match_day <= ?', @date_from_restriction.match_day, @date_to_restriction.match_day).group(:player_id).each do |row|
