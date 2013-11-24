99aafd56d9efe0388b6b16838d81082517fac14d
Simon König <koenig_simon@gmx.de>
2013-11-24 16:33:07 +0100

    .

---
+++ app/models/untyped_game.rb
@@ -0,0 +1,26 @@
+class UntypedGame
+  attr_accessor :match_day
+  attr_accessor :player, :number
+  attr_accessor :points, :cleared_frames, :strikes, :spares, :fouls, :splits, :cleared_splits
+
+  attr_accessor :frame01_result1, :frame01_result2
+  attr_accessor :frame01_state1,  :frame01_state2
+  attr_accessor :frame02_result1, :frame02_result2
+  attr_accessor :frame02_state1,  :frame02_state2
+  attr_accessor :frame03_result1, :frame03_result2
+  attr_accessor :frame03_state1,  :frame03_state2
+  attr_accessor :frame04_result1, :frame04_result2
+  attr_accessor :frame04_state1,  :frame04_state2
+  attr_accessor :frame05_result1, :frame05_result2
+  attr_accessor :frame05_state1,  :frame05_state2
+  attr_accessor :frame06_result1, :frame06_result2
+  attr_accessor :frame06_state1,  :frame06_state2
+  attr_accessor :frame07_result1, :frame07_result2
+  attr_accessor :frame07_state1,  :frame07_state2
+  attr_accessor :frame08_result1, :frame08_result2
+  attr_accessor :frame08_state1,  :frame08_state2
+  attr_accessor :frame09_result1, :frame09_result2
+  attr_accessor :frame09_state1,  :frame09_state2
+  attr_accessor :frame10_result1, :frame10_result2, :frame10_result3
+  attr_accessor :frame10_state1,  :frame10_state2,  :frame10_state3
+end
