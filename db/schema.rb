b64fcc1d666a56edd8f9ec440e1c211854268ab3
Simon König <koenig_simon@gmx.de>
2013-12-02 23:47:01 +0100

    test

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130608083116) do
+ActiveRecord::Schema.define(:version => 20131125220346) do
 
   create_table "categories", :force => true do |t|
     t.string   "name"
