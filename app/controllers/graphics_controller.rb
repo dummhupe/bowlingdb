6dd4d0ffc99856b0457c89b606a8a44943668f21
Simon König <koenig_simon@gmx.de>
2014-02-28 22:27:20 +0100

    Added new location chart with dates for testing purposes

--- app/controllers/graphics_controller.rb
+++ app/controllers/graphics_controller.rb
@@ -34,7 +34,6 @@ class GraphicsController < ReportController
       end
       if @regression and @games[player_id] and @games[player_id].to_a.size >= 5
         scores = @games[player_id]
-	puts scores
 	@regression_data[player_id] = regression(scores.map {|g| g.match_day.match_day.to_datetime.to_i }, scores.map {|g| g.points })
       end
     end       
@@ -57,11 +56,32 @@ class GraphicsController < ReportController
       end
       if @regression and @games[location_id] and @games[location_id].to_a.size >= 5
         scores = @games[location_id]
-	session['scores.size'] = scores.to_a.size-1
 	@regression_data[location_id] = regression((0..(scores.to_a.size-1)).map{|i| i}, scores.map{|g| g.points})
       end
     end
 
     render :location_chart
+  end
+
+  def player_by_location_with_dates
+    if @player_restrictions.kind_of? Array
+      @player_restrictions = @player_restrictions.first
+    end
+
+    @games = {}
+    @regression_data = {}
+    @nMaxSeriesPoints = 0
+    @location_restrictions.each do |location_id|
+      @games[location_id] = Game.joins(:match_day).select('match_day_id, COUNT(points) AS game_count, AVG(points) AS points').where(:player_id => @player_restrictions, 'match_days.location_id' => location_id, 'match_days.category_id' => @category_restrictions).where('match_days.match_day >= ? AND match_days.match_day <= ?', @date_from_restriction.match_day, @date_to_restriction.match_day).group('match_day_id').order('match_days.match_day')
+      if @games[location_id].length > @nMaxSeriesPoints
+        @nMaxSeriesPoints = @games[location_id].length
+      end
+      if @regression and @games[location_id] and @games[location_id].to_a.size >= 5
+        scores = @games[location_id]
+	@regression_data[location_id] = regression((0..(scores.to_a.size-1)).map{|i| i}, scores.map{|g| g.points})
+      end
+    end
+
+    render :location_chart_with_dates
   end
 end
