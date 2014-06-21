3ef930f4117df4390ad969b7356b47d3c7b262fa
Simon König <koenig_simon@gmx.de>
2014-06-21 10:43:09 +0200

    Added new graphics to compare different categories

--- app/controllers/graphics_controller.rb
+++ app/controllers/graphics_controller.rb
@@ -84,4 +84,48 @@ class GraphicsController < ReportController
 
     render :location_chart_with_dates
   end
+
+  def player_by_category
+    if @player_restrictions.kind_of? Array
+      @player_restrictions = @player_restrictions.first
+    end
+
+    @games = {}
+    @regression_data = {}
+    @nMaxSeriesPoints = 0
+    @category_restrictions.each do |category_id|
+      @games[category_id] = Game.joins(:match_day).select('match_day_id, COUNT(points) AS game_count, AVG(points) AS points').where(:player_id => @player_restrictions, 'match_days.location_id' => @location_restrictions, 'match_days.category_id' => category_id).where('match_days.match_day >= ? AND match_days.match_day <= ?', @date_from_restriction.match_day, @date_to_restriction.match_day).group('match_day_id').order('match_days.match_day')
+      if @games[category_id].length > @nMaxSeriesPoints
+        @nMaxSeriesPoints = @games[category_id].length
+      end
+      if @regression and @games[category_id] and @games[category_id].to_a.size >= 5
+        scores = @games[category_id]
+        @regression_data[category_id] = regression((0..(scores.to_a.size-1)).map{|i| i}, scores.map{|g| g.points})
+      end
+    end
+
+    render :category_chart
+  end
+
+  def player_by_category_with_dates
+    if @player_restrictions.kind_of? Array
+      @player_restrictions = @player_restrictions.first
+    end
+
+    @games = {}
+    @regression_data = {}
+    @nMaxSeriesPoints = 0
+    @category_restrictions.each do |category_id|
+      @games[category_id] = Game.joins(:match_day).select('match_day_id, COUNT(points) AS game_count, AVG(points) AS points').where(:player_id => @player_restrictions, 'match_days.location_id' => @location_restrictions, 'match_days.category_id' => category_id).where('match_days.match_day >= ? AND match_days.match_day <= ?', @date_from_restriction.match_day, @date_to_restriction.match_day).group('match_day_id').order('match_days.match_day')
+      if @games[category_id].length > @nMaxSeriesPoints
+        @nMaxSeriesPoints = @games[category_id].length
+      end
+      if @regression and @games[category_id] and @games[category_id].to_a.size >= 5
+        scores = @games[category_id]
+        @regression_data[category_id] = regression(scores.map {|g| g.match_day.match_day.to_datetime.to_i }, scores.map {|g| g.points })
+      end
+    end
+
+    render :category_chart_with_dates
+  end
 end
