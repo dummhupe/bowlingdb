b3ce0d17ee67a3ee0b1d5d5f3932732e716adcc0
Simon König <koenig_simon@gmx.de>
2015-05-06 13:49:03 +0200

    Improved sorting of match days for start and index page: show latest entry first

--- app/controllers/match_days_controller.rb
+++ app/controllers/match_days_controller.rb
@@ -2,11 +2,11 @@ class MatchDaysController < ApplicationController
   WillPaginate.per_page = 20
 
   def index
-    @match_days = MatchDay.order('match_day DESC').paginate(:page => params[:page])
+    @match_days = MatchDay.order('match_day DESC, id DESC').paginate(:page => params[:page])
   end
 
   def latest
-    redirect_to match_day_path(MatchDay.order('match_day DESC').first)
+    redirect_to match_day_path(MatchDay.order('match_day DESC, id DESC').first)
   end
 
   def show
